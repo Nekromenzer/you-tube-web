158704a8e87ced68a43c642f46176f851807e125
maneeshapriwoo <maneesha@priwoo.com>
2021-05-07 20:33:55 +0530

    you tube web

---
+++ README.md
@@ -0,0 +1,13 @@
+## Getting Started
+
+Install the dependencies and run the development server
+<!-- next js /React Project -->
+```bash
+npm install
+npm run dev
+# or
+yarn
+yarn dev
+```
+
+<!-- github - Nekromenzer -->
---
+++ src/data/key-feature-data.js
@@ -0,0 +1,41 @@
+
+export default [
+  {
+    id: 1,
+    imgSrc: Photography,
+    altText: 'Photography',
+    title: 'Photography',
+    text:
+      'Get your blood tests delivered at home collect a sample from the your blood tests.'
+  },
+  {
+    id: 2,
+    imgSrc: Videography,
+    altText: 'Videography',
+    title: 'Videography',
+    text:
+      'Get your blood tests delivered at home collect a sample from the your blood tests.'
+  },
+  {
+    id: 3,
+    imgSrc: Gaming,
+    altText: 'Gaming',
+    title: 'Gaming',
+    text:
+      'Get your blood tests delivered at home collect a sample from the your blood tests.'
+  },
+  {
+    id: 4,
+    imgSrc: Programming,
+    altText: 'Programming',
+    title: 'Programming',
+    text:
+      'Get your blood tests delivered at home collect a sample from the your blood tests.'
+  }
+]
+
+
+import Photography from 'assets/key-feature/photography.png'
+import Videography from 'assets/key-feature/videography.png'
+import Gaming from 'assets/key-feature/gaming.png'
+import Programming from 'assets/key-feature/programming.png'
